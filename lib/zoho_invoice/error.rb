c011252a3379734e61487532152db3619d1a089a
jer-k <jeremy@carwoo.com>
2013-04-22 12:37:17 -0700

    Add client error

--- lib/zoho_invoice/error.rb
+++ lib/zoho_invoice/error.rb
@@ -1,7 +1,6 @@
 module ZohoInvoice
-  class Error
+  class Error < StandardError
 
-    attr_accessor :message, :code
-
+    attr_accessor :message, :code, :response_status, :http_status
   end
 end
