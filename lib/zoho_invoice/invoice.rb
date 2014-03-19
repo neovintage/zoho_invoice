842200b35c4e62e92912a9f50ff6a398a6db2aec
Alex Sherstinsky <asherstinsky@freejit.com>
2014-03-18 19:49:36 -0700

    debug

--- lib/zoho_invoice/invoice.rb
+++ lib/zoho_invoice/invoice.rb
@@ -13,7 +13,7 @@ module ZohoInvoice
       :p_o_number,            :balance,
       :status,                :notes,
       :customer_id,           :terms,
-      :customer_name,         :custom_fields,
+      :customer_name,         :customfields,
       :invoice_date,
       :payments_due,
       :due_date,
