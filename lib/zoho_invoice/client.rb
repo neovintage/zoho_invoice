7424bfefed51347e7c6309d02cd329215e043968
Alex Sherstinsky <asherstinsky@freejit.com>
2014-03-19 17:07:00 -0700

    debug

--- lib/zoho_invoice/client.rb
+++ lib/zoho_invoice/client.rb
@@ -46,8 +46,8 @@ conn.put do |req|
 req.url(path)
 req.params = req.params.merge(credentials)
 #req.body = "JSONString=#{inv_json}"
-req.body = "JSONString={\"customer_id\":\"480266000000071001\", \"notes\":\"Alex Test 5\"}"
-puts("VERB=$#{verb}$ ; EXECUTING=$#{req.params}$ ; REQ.BODY=$#{req.body}$")
+req.body = "JSONString={\"customer_id\":\"480266000000071001\", \"notes\":\"Alex Test 3\"}"
+puts("VERB=$#{verb}$ ; PATH=$#{path}$ ; URL=$#{req.url}$ ; EXECUTING=$#{req.params}$ ; REQ.BODY=$#{req.body}$")
 end
 else
 connection.send(verb, path, params)
