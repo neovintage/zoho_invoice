4cdcf713f885e5744629bc2d2b101f6f67a5f6f7
Alex Sherstinsky <asherstinsky@freejit.com>
2014-03-21 00:12:39 -0700

    debug

--- lib/zoho_invoice/invoice.rb
+++ lib/zoho_invoice/invoice.rb
@@ -157,15 +157,18 @@ module ZohoInvoice
       :date,
       #
       #:description,
-      :discount,
-      :discount_type,
-      :expense_id,
-      :gateway_name,
-      :invoiced_estimate_id,
-      :is_discount_before_tax,
-      :item_id,
-      :line_items,
+      #
+      #:discount,
+      #:discount_type,
+      #:expense_id,
+      #:gateway_name,
+      #:invoiced_estimate_id,
+      #:is_discount_before_tax,
+      #:item_id,
+      #:line_items,
+      #
       #:name,
+      #
       :payment_gateways,
       :payment_options,
       :payment_terms,
