218654ce984bbdcc8be188443520d7cc8a78197c
Stijn Verrept <stijn.verrept@gmail.com>
2014-10-24 01:11:36 +0200

    Updated contact so it matches definition of Zoho, and used READ_ATTRIBUTES similar to other classes.

--- lib/zoho_invoice/contact.rb
+++ lib/zoho_invoice/contact.rb
@@ -3,12 +3,21 @@ require 'zoho_invoice/base'
 module ZohoInvoice
   class Contact < Base
 
-    define_object_attrs :salutation,
-      :first_name,
-      :last_name,
-      :e_mail,
-      :phone,
-      :mobile
+    READ_ATTRIBUTES = [
+        :contact_id,
+        :contact_name,
+        :company_name,
+        :first_name,
+        :last_name,
+        :address,
+        :email,
+        :phone,
+        :mobile
+    ]
+
+    CREATE_UPDATE_ATTRIBUTES = READ_ATTRIBUTES - [:contact_id]
+
+    define_object_attrs(*READ_ATTRIBUTES)
 
     def self.create(client, options = {})
       raise ZohoInvoice::ActionNotSupportedError
@@ -16,6 +25,14 @@ module ZohoInvoice
 
     def save
       raise ZohoInvoice::ActionNotSupportedError
+    end
+
+    def self.all(client)
+      retrieve(client, '/api/v3/contacts')
+    end
+
+    def self.find(client, id, options={})
+      retrieve(client, "/api/v3/contacts/#{id}", false)
     end
 
   end
